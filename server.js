65c0b46add31559485b92171a2a73e7f94910030
Draquix <Draquix@yahoo.com>
2021-03-02 10:32:01 -0600

    index will not render

--- server.js
+++ server.js
@@ -5,7 +5,9 @@ require('dotenv').config();
 
 // Application Dependencies
 const express = require('express');
-const cors = require('cors');
+const ejs = require('ejs');
+
+//const cors = require('cors');
 
 //const pg = require('pg');
 //const client = new pg.Client(process.env.DATABASE_URL);
@@ -19,7 +21,9 @@ const superagent = require('superagent'); //<<--will go in module
 const PORT = process.env.PORT || 3001 || 3002 || 3003;
 console.log('Crypto Server is running on port: ', PORT);
 const app = express();
+app.use(express.static('public'));
 
+app.set('view engine', 'ejs');
 // client.connect()
 //   .then(() => {
      app.listen(PORT, () => console.log(`SERVER up on PORT : ${PORT}`));
@@ -29,25 +33,30 @@ const app = express();
 // Our Dependencies - modules
 
 //server paths
-app.get('/apitest', apiTest);
+//app.get('/apitest', apiTest);
+app.get('/', homeRoute);
 
-function apiTest( req, res ) {
-  console.log('path triggered');
-  const apiKey = process.env.CRYPTO_KEY;
-  const url = "https://api.binance.com/api/v3/ticker/24hr?symbol=LTCBTC"
-  const queryParams = {
-    key:apiKey, quantity:1, price: 0.1, recvWindow:5000, timestamp:1499827319559
-  };
-  superagent.get(url)
-  //.query(queryParams)
-  .then( returned => {
-    console.log('***the body:', returned.body);
-  }).catch(error => {
-    console.log("***ERROR:", error);
-    res.status(500).send('Error In Query');
-
-  })
+function homeRoute (req, res) {
+  
+  res.render('index.ejs');
 }
+
+// function apiTest( req, res ) {
+//   console.log('path triggered');
+//   const apiKey = process.env.CRYPTO_KEY;
+//   const url = "https://api.binance.com/api/v3/ticker/24hr?symbol=LTCBTC"
+//   const queryParams = {
+//     key:apiKey, quantity:1, price: 0.1, recvWindow:5000, timestamp:1499827319559
+//   };
+//   superagent.get(url)
+//   //.query(queryParams)
+//   .then( returned => {
+//     console.log('***the body:', returned.body);
+//   }).catch(error => {
+//     console.log("***ERROR:", error);
+//     res.status(500).send('Error In Query');
+//   })
+// }
 
 
 
