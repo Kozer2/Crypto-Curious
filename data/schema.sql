5eab171e315ccfdc3f0c26023df7ec876c38fd62
jmcia2020 <jeanmmcmahon@gmail.com>
2021-03-04 09:48:09 -0600

    data going to database

--- data/schema.sql
+++ data/schema.sql
@@ -1,16 +1,17 @@
+DROP TABLE IF EXISTS crypto_currency;
 CREATE TABLE IF NOT EXISTS crypto_currency (
   id SERIAL PRIMARY KEY,
   user_name VARCHAR(255),
   symbol_name1 VARCHAR(255),
-  usd_price1 VARCHAR(255),
+  usd_price1 DOUBLE PRECISION,
   symbol_name2 VARCHAR(255),
-  usd_price2 VARCHAR(255),
+  usd_price2 DOUBLE PRECISION,
   symbol_name3 VARCHAR(255),
-  usd_price3 VARCHAR(255),
+  usd_price3 DOUBLE PRECISION,
   symbol_name4 VARCHAR(255),
-  usd_price4 VARCHAR(255),
+  usd_price4 DOUBLE PRECISION,
   symbol_name5 VARCHAR(255),
-  usd_price5 VARCHAR(255),
-  time_date_stamp VARCHAR(25),
+  usd_price5 DOUBLE PRECISION,
+  time_date_stamp TIMESTAMP,
   description TEXT
 );
